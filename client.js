d2ed8f699d28865e5dc55a77aedc32fed8aa3a13
Ayriel Ash <aya37278@bethel.edu>
2019-10-04 13:51:20 -0500

    delete working and counter update

--- client.js
+++ client.js
@@ -7,9 +7,7 @@ function onReady(){
     $('#addMeetup').on('click', addMeetup);
     //display starting meetup counter
     $('#totalMeetup').append('<p id=\"currentCount\">'+meetupCounter+'</p>');
-    //listenDelete();
-    $('.delete').on('click', listenDelete);
-
+    $('#meetupTable').on('click', '.delete', deleteRow);
 }//end onReady
 
 function addMeetup(){
@@ -24,22 +22,30 @@ function addMeetup(){
     + group + '</td><td>' + date +
     '</td><td><button class="delete">Delete</button></td></tr>';
     $('#meetupTable').append(str);
-    //add listener for new delete button
-    listenDelete();
     //increment counter and send back to p tag
     $('#currentCount').text(meetupCounter);
 
 }//end addMeetup
 
-function deleteRow(rowId){
+function deleteRow(){
     console.log('in delete row');
-    //remove row based on id
+    //get the delete row
+    //for  id base remove which is unnecessary let myRow=$(this).closest("tr").attr("id");
+    //remove row based on id $('#'+myRow).remove();
+    //remove the row from the click
+    $(this).closest("tr").remove();
+    //decrement the counter
+    meetupCounter --;
+    $('#currentCount').text(meetupCounter);
+
 }//end delete row
 
+/*unused would have to remove and readd all listeners so it doesn't duplicate listeners
 function listenDelete(){
     console.log('in listenDelete')
     //set listener for current delete button
-    //('.delete').on('click', function (){
-        //deleteRow();
-    //});
-}
+    $('.delete').on('click', function (){
+        deleteRow();
+    });
+} 
+*/
